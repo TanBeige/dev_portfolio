09c80cb89140534a909149da1325466561a52ac9
Tan <tanarin12@gmail.com>
2022-06-21 19:17:10 -0400

    Lint Fix

--- components/About.js
+++ components/About.js
@@ -44,6 +44,7 @@ export default function About() {
             </a>
             <a
             target="_blank"
+            rel="noreferrer" 
               href="https://docs.google.com/document/d/1HWcjZIydSkA_VqC8WiAN_gz5Lc3iGFW5/edit?usp=sharing&ouid=111873578985316598156&rtpof=true&sd=true"
               className=" ml-4 items-center inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
               Resume
